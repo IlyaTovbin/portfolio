23918a606dfd045044f5ca42a5db4375ab9c71c8
ilyatovbin <ilyaxiv0@gmail.com>
2025-10-27 10:25:44 -0700

    main-page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,20 @@
+import { FaUserCircle } from 'react-icons/fa';
 import '../styles/pages.css';
+import { Link } from 'react-router-dom';
 
 function Main() {
   return (
-    <div className="page main-page content-inner-main">Main Page</div>
+    <div className="page main-page">
+      <div className="shodow-layer content-inner-main">
+        <h1>Hello!</h1>
+        <div className='content-inner-main-descripition'>My name is <span className='title'>Ilya Tovbin</span>, and I'm a Frontend web developer</div>
+        <div className='learn-more pointer'>
+          <Link to="/about">
+            <FaUserCircle /> Learn more about me
+          </Link>
+          </div>
+      </div>
+    </div>
   )
 }
 
